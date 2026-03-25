3cf6f4d6051b20c378a351b2acab542a67ff483f
RafacraftCoder! <sonicfast12overload@gmail.com>
2026-03-25 08:53:33 -0700

    Update Or.java

--- src/main/java/iej/node/Or.java
+++ src/main/java/iej/node/Or.java
@@ -1,31 +1,30 @@
 package iej.node;
 
 public class Or extends Node {
-	
-	/**
-	* IEJ – Interactive Expression Java
-	* Author: RafacraftCoder
-	* GitHub: https://github.com/RafacraftDev/IEJ
-	*
-	* Description: Part of the IEJ interpreter project
-	* License: MIT
-	*/
-	
+
     private Or(String regex) {
         super(regex);
     }
 
-    public static Or of(String... options) {
-        return new Or(build(options));
+    public static Or of(Node... nodes) {
+        return new Or(build(nodes));
     }
 
-    static String build(String[] options) {
+    public static Or of(String... options) {
+        Node[] nodes = new Node[options.length];
+        for (int i = 0; i < options.length; i++) {
+            nodes[i] = Literal.of(options[i]);
+        }
+        return of(nodes);
+    }
+
+    static String build(Node[] nodes) {
         StringBuilder sb = new StringBuilder("(?:");
 
-        for (int i = 0; i < options.length; i++) {
-            sb.append(options[i]); // append option in i
-            if (i < options.length - 1) {
-                sb.append("|"); // append |
+        for (int i = 0; i < nodes.length; i++) {
+            sb.append(nodes[i].get());
+            if (i < nodes.length - 1) {
+                sb.append('|');
             }
         }
 
