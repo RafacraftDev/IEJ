43b80a85cc444587ba29018fd399d662aca4e6c1
RafacraftCoder! <sonicfast12overload@gmail.com>
2026-03-25 08:59:22 -0700

    Update Node.java

--- src/main/java/iej/node/Node.java
+++ src/main/java/iej/node/Node.java
@@ -20,6 +20,26 @@ public abstract class Node {
 	public String get() {
 		return regex; // return regex
 	}
+
+	public Node or(Node e2) {
+		return Or.of(this, e2);
+	}
+
+	public Node zeroOrMore() {
+		return ZeroOrMore.of(this);
+	}
+
+	public Node oneOrMore() {
+		return OneOrMore.of(this);
+	}
+
+	public Node optional() {
+		return Optional.of(this);
+	}
+
+	public Node optionalOr(Node e2) {
+		return OptionalOr.of(this, e2);
+	}
 	
 	@Override
 	public String toString() {
